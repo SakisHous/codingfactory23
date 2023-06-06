ffb2cb41084fb64abb83fecbede28112205d55c7
sakis <th.chousiadas@gmail.com>
2023-06-06 20:12:16 +0300

    PointApp using static factory methods

--- src/gr/aueb/cf/ch11/PointApp.java
+++ src/gr/aueb/cf/ch11/PointApp.java
@@ -7,11 +7,13 @@ public class PointApp {
         Point p2 = new Point(1, 2);
         Point p3 = new Point();
 
+        Point p4 = Point.getRandomPoint();
+
         p1.setX(3);
         p1.setY(12);
 
         System.out.println(p1.pointToString());
         System.out.println(p2.pointToString());
-        System.out.println(p3.pointToString());
+        System.out.println(p4.pointToString());
     }
 }
